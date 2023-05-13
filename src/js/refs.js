51365f782ef2d9e9543aac1eaaffb0e5caaf2fbf
molvi1998 <132092205+molvi1998@users.noreply.github.com>
2023-05-13 17:23:39 +0000

    Deploying to gh-pages from @ molvi1998/goit-js-hw-11@d9ab807839ab2a1ca74ac82b13ad6157e8eb1273 🚀

--- src/js/refs.js
+++
@@ -1,6 +0,0 @@
-export const refs = {
-    form: document.querySelector(".search-form"),
-    gallery: document.querySelector(".gallery"),
-    card: document.querySelector(".photo-card"),
-    loadMoreBtn: document.querySelector(".load-more")
-}
